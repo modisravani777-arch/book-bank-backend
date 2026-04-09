a69cef101ca9dcac80b0cf0ff2f0afe2bdb7785a
Modi Sravani <sravani@Modis-MacBook-Air.local>
2026-04-09 13:15:49 +0530

    Initial backend commit with updated db.js

--- db.js
+++ db.js
@@ -1,18 +1,19 @@
 const mysql = require("mysql2");
 
+// Use environment variables for cloud deployment
 const db = mysql.createConnection({
-host: "localhost",
-user: "root",
-password: "Sravani@123",
-database: "smart_book_bank"
+  host: process.env.MYSQL_HOST || "localhost",
+  user: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PASSWORD || "Sravani@123",
+  database: process.env.MYSQL_DATABASE || "smart_book_bank",
 });
 
 db.connect((err) => {
-if(err){
-console.log("DB Error", err);
-}else{
-console.log("MySQL Connected ✅");
-}
+  if (err) {
+    console.log("DB Error", err);
+  } else {
+    console.log("MySQL Connected ✅");
+  }
 });
 
 module.exports = db;
